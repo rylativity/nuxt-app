9951b58531da6f2d7a6bfd7131267d3a2a3c1ad5
Ryan <ryan.stewart113@gmail.com>
2022-03-26 00:07:46 -0400

    initial commit, add vuex store

---
+++ pages/inspire.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <img
+        src="/v.png"
+        alt="Vuetify.js"
+        class="mb-5"
+      >
+      <blockquote class="blockquote">
+        &#8220;First, solve the problem. Then, write the code.&#8221;
+        <footer>
+          <small>
+            <em>&mdash;John Johnson</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-col>
+    <v-col>
+      <v-card>
+        <v-btn @click="increment()">
+          Click to Increment
+        </v-btn>
+        <v-card-title>
+          Counter
+        </v-card-title>
+        <v-card-text>
+          {{ count }}
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import { mapMutations } from 'vuex';
+
+export default {
+  name: 'InspirePage',
+  computed: {
+    count() {
+      return this.$store.state.counter
+    }
+  },
+  methods: {
+    ...mapMutations({
+      increment: 'increment'
+    })
+  }
+}
+</script>
